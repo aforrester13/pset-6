fe2549665a31f6b5e84f6a00d45b7eb432617ebf
Aidan Forrester <Aidan@Daras-MacBook-Air.local>
2019-12-23 14:32:07 -0500

    watch the video

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 window.onload = function() {
 
+  document.getElementbyId("add-Item").onclick = addItem
 }
 
 let x = document.getElementbyId("add-Item");
 x.appendToEnd()
+
+const addItem = function() {
+  let textEntered = document.getElementbyId("myText")
+  let canvas = document.createElement("CANVAS")
+  
+  let ctx = canvas.getContext('2d')
+  ctx.clearRect = (0, 0, canvas.width, canvas.height)
+
+
+}

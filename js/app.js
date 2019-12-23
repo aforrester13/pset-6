22e6e229267d5ee963fbf133e3d37eefa49c5fa1
Aidan Forrester <Aidan@Daras-MacBook-Air.local>
2019-12-23 13:55:30 -0500

    second commit

--- js/app.js
+++ js/app.js
@@ -1,27 +1,6 @@
 window.onload = function() {
 
-
-  document.getElementById("add-item").onclick = addItem
 }
 
-
-const addItem = function() {
-  let canvas = document.getElementById("canvas1").onclick
-  let ctx = canvas.getContext('2d')
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-  let array = [add]
-  do {
-    var add = prompt("Add Item: ");
-    if (add == null) {
-      break;
-    }
-
-    var priority = prompt("Priority: ");
-    if (priority == null) {
-      break;
-    }
-
-
-  }
-}
+let x = document.getElementbyId("add-Item");
+x.appendToEnd()

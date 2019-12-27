14bf071d9f4030bb005ca04e4fafb7f3485db96e
Aidan Forrester <Aidan@Daras-MacBook-Air.local>
2019-12-27 12:33:09 -0500

    i'm stuck

--- js/app.js
+++ js/app.js
@@ -1,17 +1,45 @@
-window.onload = function() {
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
 
-  document.getElementbyId("add-Item").onclick = addItem
+
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+const option = {weekday : "long", month: "short", day: "numeric"}
+const today = new Date();
+
+dateElement.innerHTML = today.toLocaleDateString("en-US", options)
+
+function addToDo(toDo){
+
+  if(trash){ return; }
+
+  const DONE = done ? CHECK : UNCHECK;
+  const LINE = done ? LINE_THROUGH : "";
+
+  const item = `<li class= "item">
+                  <i class="fa ${DONE}" job="complete" id="${id}"></i>
+                  <p class="text ${LINE}">Drink Coffee</p>
+                  <i class = "fa fa-trash-o de" job= "delete" id="${id}"></i>
+                </li>
+               `;
+  const position = "beforeend";
+
+  list.insertAdjacentHTML(position, item);
 }
 
-let x = document.getElementbyId("add-Item");
-x.appendToEnd()
+document.addEventListener("keyup", function(event){
+  if(event.keyCode == 13){
+    const toDo = input.value
 
-const addItem = function() {
-  let textEntered = document.getElementbyId("myText")
-  let canvas = document.createElement("CANVAS")
-  
-  let ctx = canvas.getContext('2d')
-  ctx.clearRect = (0, 0, canvas.width, canvas.height)
+    if(toDo){
+      addToDo(toDo)
+    }
+    input.value = "",
+  }
+});
 
-
-}
+addToDo("Drink Coffee", 1, true, false);

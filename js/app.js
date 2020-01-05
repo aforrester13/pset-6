cb87fab57c4351119a66b37839bdb03ad7f1e2a2
Aidan Forrester <Aidan@Daras-MacBook-Air.local>
2020-01-05 18:07:42 -0500

    finished product

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 let elements = []
-var add = document.getElementById("add_button");
-var row = document.getElementsByClassName("row");
-let priority_button = document.getElementsByClassName("priority_button");
-let complete_button = document.getElementsByClassName("complete_button");
-let remove_button = document.getElementsByClassName("remove_button");
-let list_item = document.getElementsByClassName("to_do")
+var addButton = document.getElementById("add_button");
+var tableRow = document.getElementsByClassName("row");
+let priorityButton = document.getElementsByClassName("priority_button");
+let completeButton = document.getElementsByClassName("complete_button");
+let removeButton = document.getElementsByClassName("remove_button");
+let listItem = document.getElementsByClassName("to_do")
 
 let element_prioritize;
 
@@ -39,35 +39,35 @@ const create_item = function() {
       elements[index].htmlPriorityButton.setAttribute("class", "priority_button");
       elements[index].htmlPriorityButton.innerHTML = "!";
 
-      row[index].append(elements[index].htmlPriorityButton);
+      tableRow[index].append(elements[index].htmlPriorityButton);
 
       elements[index].htmlText = document.createElement("td");
       elements[index].htmlText.innerHTML = elements[index].task;
       elements[index].htmlText.setAttribute("class", "to_do");
 
-      row[index].append(elements[index].htmlText);
+      tableRow[index].append(elements[index].htmlText);
 
       elements[index].htmlCompleteButton = document.createElement("td");
       elements[index].htmlCompleteButton.innerHTML = "&#x2713;";
       elements[index].htmlCompleteButton.setAttribute("class", "complete_button");
 
-      row[index].append(elements[index].htmlCompleteButton);
+      tableRow[index].append(elements[index].htmlCompleteButton);
 
       elements[index].htmlRemoveButton = document.createElement("td");
       elements[index].htmlRemoveButton.setAttribute("class", "remove_button");
       elements[index].htmlRemoveButton.innerHTML = "X";
 
-      row[index].append(elements[index].htmlRemoveButton);
+      tableRow[index].append(elements[index].htmlRemoveButton);
     }
     document.getElementById("input_item").value = "";
 };
 
 const remove_item = function() {
   var removed = false;
-  for (let i = 0; i < remove_button.length; i++) {
-    remove_button[i].onclick = function() {
+  for (let i = 0; i < removeButton.length; i++) {
+    removeButton[i].onclick = function() {
         removed = true;
-        let remove_element = row[i];
+        let remove_element = tableRow[i];
         remove_element.remove();
         elements.splice(i, 1);
     };
@@ -79,22 +79,22 @@ const remove_item = function() {
 
 const finish_item = function() {
   var finish = false;
-  for (let x = 0; x < complete_button.length; x++) {
-    complete_button[x].onclick = function() {
+  for (let x = 0; x < completeButton.length; x++) {
+    completeButton[x].onclick = function() {
        if (elements[x].complete == false) {
          finish = true;
-         list_item[x].style.setProperty("text-decoration", "line-through");
-         list_item[x].style.backgroundColor = "#baff66";
-         complete_button[x].style.backgroundColor = "#baff66";
-         list_item[x].style.color = "black";
+         listItem[x].style.setProperty("text-decoration", "line-through");
+         listItem[x].style.backgroundColor = "#baff66";
+         completeButton[x].style.backgroundColor = "#baff66";
+         listItem[x].style.color = "black";
          elements[x].complete = true;
        }
        else if (elements[x].complete == true) {
-         complete_button[x].style.backgroundColor = "black";
-         complete_button[x].style.color = "#A4FFA4"
-         list_item[x].style.setProperty("text-decoration", "none");
-         list_item[x].style.backgroundColor = "black";
-         list_item[x].style.color = "#A4FFA4";
+         completeButton[x].style.backgroundColor = "black";
+         completeButton[x].style.color = "#A4FFA4"
+         listItem[x].style.setProperty("text-decoration", "none");
+         listItem[x].style.backgroundColor = "black";
+         listItem[x].style.color = "#A4FFA4";
          elements[x].complete = false;
        }
      };
@@ -106,15 +106,15 @@ const finish_item = function() {
 
 const prioritize_item = function() {
   var prioritize = false;
-  for (let z = 0; z < priority_button.length; z++) {
-    priority_button[z].onclick = function () {
+  for (let z = 0; z < priorityButton.length; z++) {
+    priorityButton[z].onclick = function () {
       if (elements[z].priority == false) {
-        element_prioritize = row[z]
+        element_prioritize = tableRow[z]
         prioritize = true;
-        priority_button[z].style.backgroundColor = "#fff98a";
-        priority_button[z].style.color = "black";
-        priority_button[z].style.borderColor = "black";
-        row[0].before(element_prioritize);
+        priorityButton[z].style.backgroundColor = "#fff98a";
+        priorityButton[z].style.color = "black";
+        priorityButton[z].style.borderColor = "black";
+        tableRow[0].before(element_prioritize);
         elements[z].priority = true;
 
         const objectToMove = elements[z];
@@ -124,11 +124,11 @@ const prioritize_item = function() {
         prioritize = true;
       }
       else if (elements[z].priority) {
-        element_prioritize = row[z]
-        priority_button[z].style.backgroundColor = "black";
-        priority_button[z].style.color = "#A4FFA4";
-        priority_button[z].style.borderColor = "#A4FFA4";
-        row[elements.length - 1].after(element_prioritize);
+        element_prioritize = tableRow[z]
+        priorityButton[z].style.backgroundColor = "black";
+        priorityButton[z].style.color = "#A4FFA4";
+        priorityButton[z].style.borderColor = "#A4FFA4";
+        tableRow[elements.length - 1].after(element_prioritize);
         elements[z].priority = false;
 
         let element_move = elements[z];
@@ -149,7 +149,7 @@ const startup = function() {
   prioritize_item();
 }
 
-add.onclick = create_item
+addButton.onclick = create_item
 
 document.getElementById("input_item").addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
